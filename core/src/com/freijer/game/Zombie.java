8f8ce5675acc33a8098097882a509e1733991c4d
Freijer <dm.polikarpov@gmail.com>
2020-04-11 14:27:44 +0300

    рабочая1121212

--- core/src/com/freijer/game/Zombie.java
+++ core/src/com/freijer/game/Zombie.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.math.Intersector;
 import java.awt.Rectangle;
 import java.util.Random;
 
+import javax.swing.JButton;
 import javax.xml.soap.Text;
 
 
@@ -27,97 +28,95 @@ import javax.xml.soap.Text;
 
 
 public class Zombie extends ApplicationAdapter {
-	SpriteBatch batch;
-	int zombieStateFlag = 0;
-	Texture background;
-	Texture forestBack;
-	Texture sky;
+	private SpriteBatch batch;
+	private int zombieStateFlag = 0;
+	private Texture background;
+	private Texture forestBack;
+	private Texture sky;
 
-	Texture[] zombie;
-	float jumpHeigh;
-	float jumpSpeed = 0;
-	int gameStatFlag = 0;
-	Texture manFire;
-	Texture manWeapon;
-	int mansNumber = 5;
-	float manFireX[] = new float[mansNumber];
-	float manWeaponX[] = new float[mansNumber];
-	float distanceBetweenMans;
-	float mansShift[] = new float[mansNumber];
-	Random random;
-	Random randomSpeed;
-	int manSpeed = 4;
-	Circle zombieCircle;
-	Circle[] fireAngle;
-	Circle[] weapAngle;
-	int gameScore = 0;
-	int passedIndex = 0;
-	BitmapFont scoreFont;
-	Texture gameOver;
+	private Texture[] zombie;
+	private float jumpHeigh;
+	private float jumpSpeed = 0;
+	private int gameStatFlag = 0;
+	private Texture manFire;
+	private Texture manWeapon;
+	private int mansNumber = 5;
+	private float manFireX[] = new float[mansNumber];
+	private float manWeaponX[] = new float[mansNumber];
+	private float distanceBetweenMans;
+	private float mansShift[] = new float[mansNumber];
+	private Random random;
+	private Random randomSpeed;
+	private int manSpeed = 4;
+	private Circle zombieCircle;
+	private Circle[] fireAngle;
+	private Circle[] weapAngle;
+	private int gameScore = 0;
+	private int passedIndex = 0;
+	private BitmapFont scoreFont;
+	private Texture gameOver;
 
-	Texture citzen;
-	int citzenNumber = 4;
-	int citzenSpeed = 5;
-	float citzenX[] = new float[citzenNumber];
-	float distanceCitzens;
-	float citzenShift[] = new float[citzenNumber];
-	Circle[] citzAngle;
+	private Texture citzen;
+	private int citzenNumber = 4;
+	private int citzenSpeed = 5;
+	private float citzenX[] = new float[citzenNumber];
+	private float distanceCitzens;
+	private float citzenShift[] = new float[citzenNumber];
+	private Circle[] citzAngle;
 
-	int eatScore = 0;
-	int passedCitzens = 0;
-	int zaq;
-	int eatFlag = 0;
-	int HP = 1;
-//-------
-	Texture castle;
-	int castleNumber = 2;
-	int castleSpeed = 1;
-	float castleX[] = new float[castleNumber];
-	float distancecastle;
-	//---
-	Texture ground;
-	int groundNumber = 3;
-	float groundSpeed = 0.5f;
-	float groundX[] = new float[groundNumber];
-	float distanceground;
+	private int eatScore = 0;
+	private int passedCitzens = 0;
+	private int zaq;
+	private int eatFlag = 0;
+	private int HP = 1;
+//---
+	private Texture castle;
+	private int castleNumber = 2;
+	private int castleSpeed = 1;
+	private float castleX[] = new float[castleNumber];
+	private float distancecastle;
+//---
+	private Texture ground;
+	private int groundNumber = 3;
+	private float groundSpeed = 0.5f;
+	private float groundX[] = new float[groundNumber];
+	private float distanceground;
 //--
-	Texture ground2;
-	int groundNumber2 = 3;
-	float groundSpeed2 = 0.5f;
-	float groundX2[] = new float[groundNumber];
-	float distanceground2;
+	private Texture ground2;
+	private int groundNumber2 = 3;
+	private float groundSpeed2 = 0.5f;
+	private float groundX2[] = new float[groundNumber];
+	private float distanceground2;
 //--
-	Texture tree;
-	int treeNumber = 3;
-	int treeSpeed = 1;
-	float treeX[] = new float[treeNumber];
-	float distanceTree;
-	//---
-	Texture godzilla;
-	int godzillaNumber = 3;
-	int godzillaSpeed = 1;
-	float godzillaX[] = new float[godzillaNumber];
-	float distanceGodzilla;
+	private Texture tree;
+	private int treeNumber = 3;
+	private int treeSpeed = 1;
+	private float treeX[] = new float[treeNumber];
+	private float distanceTree;
+//---
+	private Texture godzilla;
+	private int godzillaNumber = 3;
+	private int godzillaSpeed = 1;
+	private float godzillaX[] = new float[godzillaNumber];
+	private float distanceGodzilla;
 //--
-	Texture corax;
-	int coraxNumber = 3;
-	int coraxSpeed = 6;
-	float coraxX[] = new float[coraxNumber];
-	float coraxShift[] = new float[coraxNumber];
-	float distanceCorax;
-
-
-
-//------
-
-
+    private Texture corax;
+	private int coraxNumber = 3;
+	private int coraxSpeed = 6;
+	private float coraxX[] = new float[coraxNumber];
+	private float coraxShift[] = new float[coraxNumber];
+	private float distanceCorax;
+//--
+	private Texture BackTrees;
+//---
+	private int CitInvis = 0;
 
 	//ShapeRenderer shapeRenderer;
 
 
-
 	@Override
 	public void create() {
+
 		batch = new SpriteBatch();
 		background = new Texture("country1.png");
 		forestBack = new Texture("ForestBack.png");
@@ -128,8 +127,7 @@ public class Zombie extends ApplicationAdapter {
 		sky = new Texture("sky.png");
 		godzilla = new Texture("godzilla.png");
 		corax = new Texture("corax.png");
-
-
+		BackTrees = new Texture("BackTrees.png");
 
 		//shapeRenderer = new ShapeRenderer();
 		zombieCircle = new Circle();
@@ -165,8 +163,6 @@ public class Zombie extends ApplicationAdapter {
 		distanceCorax = Gdx.graphics.getWidth() + 900;
 
 		InitGame();
-
-
 	}
 
 	public void InitGame() {
@@ -188,7 +184,6 @@ public class Zombie extends ApplicationAdapter {
 		}
 		for (int k = 0; k < castleNumber; k++) {
 			castleX[k] = 700 + Gdx.graphics.getWidth() + k * distancecastle; //растояние между первым и вторям отр
-
 		}
 		for (int t = 0; t < groundNumber; t++) {
 			groundX[t] = 1; //растояние между первым и вторям отр
@@ -212,84 +207,56 @@ public class Zombie extends ApplicationAdapter {
 
 	}
 
+
+
+	public void CitzenDrawEaten(){
+		for (int c = 0; c < citzenNumber; c++) {
+			if (citzenX[c] < -citzen.getWidth()) {
+				citzenX[c] = citzenNumber * distanceCitzens;
+			} else {
+				citzenX[c] -= citzenSpeed;
+			}
+			batch.draw(citzen, citzenX[c]+200, Gdx.graphics.getHeight() / 10 + citzenShift[c]); //жертва
+//			citzAngle[c] = new Circle(citzenX[c], Gdx.graphics.getHeight() / 10 + citzenX[c], 100);
+		}
+	}
+
+	public void CitzenDraw(){
+		for (int c = 0; c < citzenNumber; c++) {
+			if (citzenX[c] < -citzen.getWidth()) {
+				citzenX[c] = citzenNumber * distanceCitzens;
+			} else {
+				citzenX[c] -= citzenSpeed;
+			}
+			batch.draw(citzen, citzenX[c], Gdx.graphics.getHeight() / 10 + citzenShift[c]); //жертва
+		}
+	}
+
+
+
+
 	@Override
 	public void render() {
 		batch.begin();
-		//batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(sky, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-		for (int g = 0; g < godzillaNumber; g++) { // годира
-			if (godzillaX[g] < -godzilla.getWidth()) {
-				godzillaX[g] = godzillaNumber * distanceGodzilla;
-			} else {
-				godzillaX[g] -= godzillaSpeed;
-				//manFireX[i] -= manSpeed[i];
-			}
-			batch.draw(godzilla, godzillaX[g], 900);
-			//fireAngle[i] = new Circle(manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i], 100);
-		}
-
-		batch.draw(forestBack, 0, -370, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-		//--------фон
-
-		for (int k = 0; k < castleNumber; k++) {
-			if (castleX[k] < -castle.getWidth()) {
-				castleX[k] = castleNumber * distancecastle;
-			} else {
-				castleX[k] -= castleSpeed;
-				//manFireX[i] -= manSpeed[i];
-			}
-			batch.draw(castle, castleX[k], Gdx.graphics.getHeight() / 10);
-			//fireAngle[i] = new Circle(manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i], 100);
-		}
-
-		for (int t = 0; t < groundNumber; t++) {
-			if (groundX[t] < -ground.getWidth()) {
-				groundX[t] = groundNumber+ distanceground;
-			} else {
-				groundX[t] -= groundSpeed;
-				//manFireX[i] -= manSpeed[i];
-			}
-			batch.draw(ground, groundX[t], 0);
-			//fireAngle[i] = new Circle(manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i], 100);
-		}
-
-		for (int t2 = 0; t2 < groundNumber2; t2++) {
-			if (groundX2[t2] < -ground2.getWidth()) {
-				groundX2[t2] = groundNumber2+ distanceground2;
-			} else {
-				groundX2[t2] -= groundSpeed2;
-				//manFireX[i] -= manSpeed[i];
-			}
-			batch.draw(ground2, groundX2[t2], 5);
-			//fireAngle[i] = new Circle(manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i], 100);
-		}
-
-		batch.draw(zombie[zombieStateFlag], 150, jumpHeigh);
-
-		for (int tr = 0; tr < treeNumber; tr++) {
-			if (treeX[tr] < -tree.getWidth()) {
-				treeX[tr] = groundNumber2+ distanceground2;
-			} else {
-				treeX[tr] -= treeSpeed;
-				//manFireX[i] -= manSpeed[i];
-			}
-			batch.draw(tree, treeX[tr], Gdx.graphics.getHeight() / 12);
-			//fireAngle[i] = new Circle(manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i], 100);
-		}
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
 
 
-//-----кончился фон
-		if (gameStatFlag == 1) {
-			if (eatFlag == 3) {
+
+		if (gameStatFlag == 1) { //общий цикл игры
+
+			if (eatFlag == 3) {// поедание жекртвы
 				if (Gdx.input.justTouched()) {
 					eatScore = eatScore +1;
 					Gdx.app.log("Пожраль!!!!", String.valueOf(eatScore));
+					CitInvis = -150;
 					eatFlag = 0;
 				}
+			} else if (eatFlag == 0 ){
+				CitInvis = 0;
 			}
+
+
 			if (manFireX[passedIndex] < Gdx.graphics.getWidth() / 30) {
 				gameScore++;
 				if (passedIndex < mansNumber - 1) {
@@ -307,16 +274,81 @@ public class Zombie extends ApplicationAdapter {
 				}
 			}
 		}
+
 		if (gameStatFlag == 1) {
 
+//			if (eatFlag == 1) {
+//				CitzenDraw();
+//			} else if (eatFlag == 3){
+//				CitzenDrawEaten();
+//				eatFlag = 1;
+//			}
 
+
+
+
+
+			batch.draw(sky, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			for (int g = 0; g < godzillaNumber; g++) { // годира
+				if (godzillaX[g] < -godzilla.getWidth()) {
+					godzillaX[g] = godzillaNumber * distanceGodzilla;
+				} else {
+					godzillaX[g] -= godzillaSpeed;
+					//manFireX[i] -= manSpeed[i];
+				}
+				batch.draw(godzilla, godzillaX[g], 900);
+				//fireAngle[i] = new Circle(manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i], 100);
+			}
+
+			batch.draw(forestBack, 0, -370, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			batch.draw(BackTrees, 0, -40, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+//--------фон
+			for (int k = 0; k < castleNumber; k++) {
+				if (castleX[k] < -castle.getWidth()) {
+					castleX[k] = castleNumber * distancecastle;
+				} else {
+					castleX[k] -= castleSpeed;
+				}
+				batch.draw(castle, castleX[k], Gdx.graphics.getHeight() / 10);
+			}
+
+			for (int t = 0; t < groundNumber; t++) {
+				if (groundX[t] < -ground.getWidth()) {
+					groundX[t] = groundNumber+ distanceground;
+				} else {
+					groundX[t] -= groundSpeed;
+				}
+				batch.draw(ground, groundX[t], 0);
+			}
+
+			for (int t2 = 0; t2 < groundNumber2; t2++) {
+				if (groundX2[t2] < -ground2.getWidth()) {
+					groundX2[t2] = groundNumber2+ distanceground2;
+				} else {
+					groundX2[t2] -= groundSpeed2;
+				}
+				batch.draw(ground2, groundX2[t2], 5);
+			}
+
+			batch.draw(zombie[zombieStateFlag], 150, jumpHeigh);
+
+			for (int tr = 0; tr < treeNumber; tr++) {
+				if (treeX[tr] < -tree.getWidth()) {
+					treeX[tr] = groundNumber2+ distanceground2;
+				} else {
+					treeX[tr] -= treeSpeed;
+				}
+				batch.draw(tree, treeX[tr], Gdx.graphics.getHeight() / 12);
+			}
+
+//-----кончился фон
 
 			for (int i = 0; i < mansNumber; i++) {
 				if (manFireX[i] < -manFire.getWidth()) {
 					manFireX[i] = mansNumber * distanceBetweenMans;
 				} else {
 					manFireX[i] -= manSpeed;
-					//manFireX[i] -= manSpeed[i];
 				}
 				batch.draw(manFire, manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i]);
 				fireAngle[i] = new Circle(manFireX[i], Gdx.graphics.getHeight() / 9 + mansShift[i], 100);
@@ -327,21 +359,36 @@ public class Zombie extends ApplicationAdapter {
 					manWeaponX[j] = mansNumber * distanceBetweenMans;
 				} else {
 					manWeaponX[j] -= manSpeed;
-					//manWeaponX[j] -= manSpeed[j];
 				}
 				batch.draw(manWeapon, manWeaponX[j], Gdx.graphics.getHeight() / 10 + mansShift[j]);
 				weapAngle[j] = new Circle(manWeaponX[j], Gdx.graphics.getHeight() / 10 + mansShift[j], 100);
 			}
 
-			for (int c = 0; c < citzenNumber; c++) {
+          //CitzenDraw();
+
+
+
+
+			for (int c = 0; c < citzenNumber; c++) {  //женртва скрыто  - вынесено в отдльеный метод
 				if (citzenX[c] < -citzen.getWidth()) {
 					citzenX[c] = citzenNumber * distanceCitzens;
 				} else {
 					citzenX[c] -= citzenSpeed;
 				}
-				batch.draw(citzen, citzenX[c], Gdx.graphics.getHeight() / 10 + citzenShift[c]);
+				if (eatFlag == 0) {
+					batch.draw(citzen, citzenX[c] + CitInvis, Gdx.graphics.getHeight() / 10 + citzenShift[c]); //жертва   // вынес
+		//			batch.draw(citzen, citzenX[c], Gdx.graphics.getHeight() / 10 + citzenShift[c]); //жертва   // вынес
+				}
+//				batch.draw(citzen, citzenX[c]+300, Gdx.graphics.getHeight() / 10 + citzenShift[c]); //жертва   // вынес
+
+
+
+
+
+
 				citzAngle[c] = new Circle(citzenX[c], Gdx.graphics.getHeight() / 10 + citzenX[c], 100);
 			}
+
 
 
 			for (int cr = 0; cr < coraxNumber; cr++) {
@@ -351,7 +398,6 @@ public class Zombie extends ApplicationAdapter {
 					coraxX[cr] -= coraxSpeed;
 				}
 				batch.draw(corax, 900-coraxX[cr], 900 + coraxShift[cr]);
-
 			}
 
 
@@ -374,12 +420,14 @@ public class Zombie extends ApplicationAdapter {
 
 
 		} else if (gameStatFlag == 2) {
+
 			batch.draw(gameOver, Gdx.graphics.getWidth() / 2 - gameOver.getWidth() / 2, Gdx.graphics.getHeight() / 2 - gameOver.getHeight() / 2);
 			if (Gdx.input.justTouched()) {
-				Gdx.app.log("tap", "ПРЫЖОК");
+				Gdx.app.log("tap", "Гамовер");
 				gameStatFlag = 1;
 				InitGame();
 				gameScore = 0;
+				eatScore = 0;
 				passedIndex = 0;
 				jumpSpeed = 0;
 			}
