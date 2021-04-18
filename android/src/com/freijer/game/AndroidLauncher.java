fe5030b219bd5ebe0c056227ee68f73b3e5e7bad
Freijer <dm.polikarpov@gmail.com>
2021-04-18 10:08:17 +0300

    Zombie 18.4.21

--- android/src/com/freijer/game/AndroidLauncher.java
+++ android/src/com/freijer/game/AndroidLauncher.java
@@ -1,16 +1,22 @@
 package com.freijer.game;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.freijer.game.Zombie;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Zombie(), config);
+		initialize(new ZombieProgres(), config);
+
+//		startActivity(new Intent(AndroidLauncher.this, Start.class));
+//		Intent intentResult = new Intent(this, Start.class);
+//		startActivity(intentResult);
 	}
 }
